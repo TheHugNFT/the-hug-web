152d401d84a86f480aacc98c94841e9fb6c240a1
Joliehugs <104857200+Joliehugs@users.noreply.github.com>
2022-06-16 17:11:42 -0700

    Update TeamMembers.js

--- website-config/TeamMembers.js
+++ website-config/TeamMembers.js
@@ -6,13 +6,13 @@ const bios = {
     otto: `I transform the world with creativity through technology and love.
            I have 20 years experience building apps, websites, and crypto stuff, 
            and helping companies implement innovative software and technology solutions`,
-    joele: `I create empowerment for others. Besides empowering teenagers in Science and Math, 
-    my favorite subject, Embrace the Hug, #thefriendshipnft, empowers everyone.`,
+    jolie: `I create connection for others. Not only do I connect teenagers with Science and Math, 
+            my favorite subjects, I connect humans with hugging and humans with web3.`
     javier: `Javier Alvarez Freelance graphic designer and Illustrator with more than 7 years 
-    of experience, lover of art and innovation, dedicated to constant learning and 
-    serving others. He loves challenges and turning his clients' ideas into visible reality. 
-    In his spare time Javier enjoys road cycling and adventure trips that enrich his art. 
-    Your goal? Change the world one design at a time.`,
+            of experience, lover of art and innovation, dedicated to constant learning and 
+            serving others. He loves challenges and turning his clients' ideas into visible reality. 
+            In his spare time Javier enjoys road cycling and adventure trips that enrich his art. 
+            His goal? Change the world one design at a time.`,
 
 }
 
@@ -25,13 +25,13 @@ export default function TeamMembers() {
             <SimpleGrid columns={[2, null, 3]} spacing={5}>
                 <TeamMember
                     name="Otto"
-                    title="Developer and CEO"
+                    title="Co-Creator and Developer"
                     src="/profile_otto.PNG"
                     bio={bios.otto}
                 />
                 <TeamMember
-                    name="Joele"
-                    title="Investor Relations, Marketing"
+                    name="Jolie"
+                    title="Co-Creator and Marketing"
                     // I learned how to drive on a manual transmission.
                     bio={bios.joele}
                     src={'/profile_joele.PNG'}
