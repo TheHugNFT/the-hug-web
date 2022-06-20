a081b748c2739e218d492124979306b19bf8e0b7
vonwao <vonwao@gmail.com>
2022-06-20 12:20:32 -0700

    improve website

--- website-config/About.js
+++ website-config/About.js
@@ -18,6 +18,16 @@ export default function About() {
                 Our Mission: Hug each address on the Ethereum network.
             </Heading>
             <Para fontSize="l">
+                The Hug Nft is a generative collection of art of humans hugging, inclusive of the full spectrum
+                of human diversity. The skin color,
+                clothing style, and backgrounds are blockchain generative traits.  When you mint one, you get a set of
+                two that are similar except for the background.  Then you send one to a friend to hug them.
+                We co-created The HUG NFT to inspire that same feeling that IRL hugging makes.
+                We felt that some of the NFT space was about money and we want some of the space to be
+                about human connection.  In addition, we host IRL events for hugging. We are having a
+                pop up hug circle this Thursday (6/23/22) in Time Square.
+            </Para>
+            <Para fontSize="l">
                 Hugs are symbolic of friendship and love and meet human needs in a variety of ways. Now
                 more than ever, hugs are important. When you buy The HUG NFT, you are hugging a friend on the blockchain.
                 Hugs connect the world.
