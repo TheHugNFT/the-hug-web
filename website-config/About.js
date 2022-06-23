4efea660141a1ab44ae899f7ef8e654255ba9969
Joliehugs <104857200+Joliehugs@users.noreply.github.com>
2022-06-23 09:45:41 -0400

    Update About.js

--- website-config/About.js
+++ website-config/About.js
@@ -18,30 +18,30 @@ export default function About() {
                 Our Mission: Hug each address on the Ethereum network.
             </Heading>
             <Para fontSize="l" pb={5}>
-                The HUG NFT is a generative collection of art of humans hugging, inclusive of the full spectrum
+                The HUG NFT is a grass roots NFT that started in Venice, CA. The HUG NFT is about human connection IRL and on the block chain.
+                Want a hug?
+                We believe that connection is more important than ever. 
+               CURRENTLY, we are building community. We felt isolated and alone during covid.   looking to build The HUG NFT is a generative collection of art of humans hugging, inclusive of the full spectrum
                 of human diversity. The skin color,
                 clothing style, and backgrounds are blockchain generative traits.  When you mint one, you get a set of
                 two that are similar except for the background.  Then you send one to a friend to hug them.
                 We co-created The HUG NFT to inspire that same feeling that IRL hugging makes.
                 We felt that some of the NFT space was about money and we want some of the space to be
                 about human connection.  In addition, we host IRL events for hugging. We are having a
-                pop up hug circle this Thursday (6/23/22) in Time Square.
+                pop up hug circle this Thursday (6/23/22) in Time Squares.
             </Para>
             <Heading size="m" fontSize={'18px'} pb={1}>
-                Jolie's Story
+                Jolie's Hug Story
             </Heading>
             <Para fontSize="l">
                 Jolie has been on a lifelong  journey of belonging through  hugging and human connection.
                 She has always felt more connected and joyful after hugging starting as a young girl.
                 She took a meditation course and learned about social capital and how hugging and human connection
-                can regulate one’s emotions.  She met Otto at a 3 day authentic relating retreat where there was
+                egulate emotions.  She met Otto at a 3 day authentic relating retreat where there was
                 a lot of hugging and human connection.  Jolie was introduced to web3 after she was
-                day trading stocks that were ‘mining’.  She couldn’t understand how tech was mined and began
+                day trading stocks that were ‘mining’ and began
                 researching mining and blockchains.  Jolie co-created The HUG NFT to inspire that same feeling
-                that IRL hugging makes.  Jolie felt that some of the NFT space was about money and she wanted
-                some of the space to be about human connection.  The HUG NFT is also about creating pop up hug
-                circles and IRL spaces for people to connect in while they are at conferences such as NFY NYC.
-                Follow TheHUGNFT on twitter for pop up hug circles this week in NYC.
+                that IRL hugging makes. 
             </Para>
             <Para fontSize="l" pt={6}>
                 Hugs are symbolic of friendship and love and meet human needs in a variety of ways. Now
